17dbe78078fdd30dcd8c57d4a5bc2bcaa30e6d88
Harish <harish.venkat2018@gmail.com>
2025-01-18 17:51:50 +0530

    data fetching in blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -18,24 +18,25 @@ async function getData() {
 async function Blog() {
   const data = await getData();
 
+  console.log("============>", data);
+
   if (data === undefined) {
     return <h1>Data not found</h1>;
   }
 
   return (
     <div className={styles.mainContainer}>
-      {data &&
-        data?.map((item) => (
-          <Link href={`/blog/${item._id}`} className={styles.container} key={item._id}>
-            <div className={styles.imgContainer}>
-              <Image src={item.img} alt="blog image" width={400} height={250} className={styles.img} />
-            </div>
-            <div className={styles.content}>
-              <h1 className={styles.title}>{item.title}</h1>
-              <p className={styles.desc}>{item.desc}</p>
-            </div>
-          </Link>
-        ))}
+      {data?.map((item) => (
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item._id}>
+          <div className={styles.imgContainer}>
+            <Image src={item.img} alt="blog image" width={400} height={250} className={styles.img} />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
